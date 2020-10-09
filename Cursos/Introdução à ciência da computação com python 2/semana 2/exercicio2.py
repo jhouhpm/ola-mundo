aacef76c49a6a939a85b638bcf0a1cabc3046965
jhouhpm <jhonatan.mello@outlook.com.br>
2020-10-08 22:08:19 -0300

    subindo novo eericio
    novo exericio

---
+++ Cursos/Introdução à ciência da computação com python 2/semana 2/exercicio2.py
@@ -0,0 +1,8 @@
+'''
+escreva uma função menor_nome(nomes) que recebe uma lista de strings com nome de pessoas como parâmetro e devolve o nome mais curto presente na lista.
+A função deve ignorar espaços antes e depois do nome e deve devolver o menor nome presente na lista. Este nome deve ser devolvido com a primeira letra maiúscula e seus demais caracteres minúsculos, independente de como tenha sido apresentado na lista passada para a função.
+Quando houver mais de um nome com o menor comprimento dentre os nomes na lista, a função deve devolver o primeiro nome com o menor comprimento presente na lista.
+'''
+
+def menor_nome(nomes):
+    
