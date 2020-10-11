9a1a5f74a162ad1599f4b01b848e61f5a087d772
jhouhpm <jhonatan.mello@outlook.com.br>
2020-10-11 19:12:56 -0300

    semana 2
    exercicios da semana 2 do segundo curso

--- Cursos/Introdução à ciência da computação com python 2/semana 2/exercicio1.py
+++ Cursos/Introdução à ciência da computação com python 2/semana 2/exercicio1.py
@@ -2,17 +2,10 @@
 Escreva a função maiusculas(frase) que recebe uma frase (uma string) como parâmetro e devolve uma string com as letras maiúsculas que existem nesta frase, na ordem em que elas aparecem.
 '''
 
+
 def maiusculas(frase):
-    string1 = ''
-    for c in string:
+    string = ''
+    for c in frase:
         if ord(c) in range(64, 91):
-            string1 += c
-    return(string1)
-
-'''escreva uma função menor_nome(nomes) que recebe uma lista de strings com nome de pessoas como parâmetro e devolve o nome mais curto presente na lista -ignorar espaços em branco.'''
-
-nomes = ['maria',' jose',' anA ']
-def menor_nome(nomes):
-    for nome in nomes:
-        nome.strip()
-        nome.ca
+            string += c
+    return(string)
--- Cursos/Introdução à ciência da computação com python 2/semana 2/exercicio2.py
+++ Cursos/Introdução à ciência da computação com python 2/semana 2/exercicio2.py
@@ -1,8 +1,10 @@
-'''
-escreva uma função menor_nome(nomes) que recebe uma lista de strings com nome de pessoas como parâmetro e devolve o nome mais curto presente na lista.
-A função deve ignorar espaços antes e depois do nome e deve devolver o menor nome presente na lista. Este nome deve ser devolvido com a primeira letra maiúscula e seus demais caracteres minúsculos, independente de como tenha sido apresentado na lista passada para a função.
-Quando houver mais de um nome com o menor comprimento dentre os nomes na lista, a função deve devolver o primeiro nome com o menor comprimento presente na lista.
-'''
+'''escreva uma função menor_nome(nomes) que recebe uma lista de strings com nome de pessoas como parâmetro e devolve o nome mais curto presente na lista -ignorar espaços em branco.'''
+
 
 def menor_nome(nomes):
-    
+    normalize_nomes = []
+    for nome in nomes:
+        normalize_nomes.append(nome.replace(' ','').capitalize())
+    menor_nome = min(normalize_nomes, key=lambda x: len(x))
+    return menor_nome
+
